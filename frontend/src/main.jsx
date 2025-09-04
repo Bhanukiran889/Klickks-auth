af49b5efddc69d424c61e341e185fba1fa0e61c9
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-09-04 19:58:57 +0530

    keep backend url in .env

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 import "./index.css";
 
 //global axios defaults
-axios.defaults.baseURL = "http://localhost:5000/auth";
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL ||  "http://localhost:5000/auth";
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(
