8e2625541300b8f74f57ec85bd4fd8a052496edc
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-09-04 19:22:27 +0530

    change navigation login in dashborad.jsx

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -5,38 +5,42 @@ import { useNavigate } from "react-router-dom";
 const Dashboard = () => {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
+
   useEffect(() => {
     axios
       .get("/dashboard")
       .then((res) => setUser(res.data.user))
-      .catch(() => navigate("./login"));
+      .catch((error) => {
+        if (error.response && error.response.status === 401) {
+          navigate("/login", { replace: true });
+        }
+      });
   }, [navigate]);
 
   const handleLogout = async () => {
     await axios.post("/logout");
-    navigate("/login");
+    navigate("/login", { replace: true });
   };
+
   return (
-    <>
-      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-        {user ? (
-          <div className="bg-white p-8 rounded-2xl shadow-lg w-96 text-center">
-            <h2 className="text-2xl font-bold mb-4">Welcome</h2>
-            <p className="mb-6">
-              You are logged in as <b>{user.email}</b>
-            </p>
-            <button
-              onClick={handleLogout}
-              className="w-full bg-red-500 text-white py-2 rounded-lg hover:bg-red-600 transition"
-            >
-              Logout
-            </button>
-          </div>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-    </>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      {user ? (
+        <div className="bg-white p-8 rounded-2xl shadow-lg w-96 text-center">
+          <h2 className="text-2xl font-bold mb-4">Welcome</h2>
+          <p className="mb-6">
+            You are logged in as <b>{user.email}</b>
+          </p>
+          <button
+            onClick={handleLogout}
+            className="w-full bg-red-500 text-white py-2 rounded-lg hover:bg-red-600 transition"
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
   );
 };
 
